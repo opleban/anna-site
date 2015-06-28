bb6fd2d371134ceffc9faec7ce383dabb4172cd4
Ori Pleban <opleban@gmail.com>
2015-06-28 02:13:42 -0400

    Adds a boatload of additional dependencies for markers and etc..

--- _posts/2015-06-27-another-post-redux.md
+++ _posts/2015-06-27-another-post-redux.md
@@ -9,7 +9,6 @@ lng: "33.025395"
 lat: "34.698867"
 image: "https://lh3.googleusercontent.com/vAN0uXNtnDESrwn4aobVzAAKi9VpWX5sd2y1Uw5RhA=w1620-h911-no"
 tags: 
-  - trekking
   - team member updates
 ---
 
--- js/site.js
+++ js/site.js
@@ -1,68 +1,107 @@
 ---
 ---
-;{% include js/jquery.min.js %}
+;{% include js/underscore.min.js %}
+;{% include js/leaflet-src.js %}
+;{% include js/Leaflet.AwesomeMarkers.min.js %}
 
 var SITE_PROP_LIB = {
   baseurl: {% raw %}"{{site.baseurl}}"{% endraw %}
 }
 
-var AnnaPostMap = function (){
-  this.map;
-  this.data = {
-    type: 'FeatureCollection',
-    features: [
-      {% for post in site.posts %}{
-        'type': 'Feature',
-        'properties': {
-          title: "{{ post.title }}",
-          image: processImageLink('{{post.image }}'),
-          link: '{{site.baseurl}}{{post.url}}',
-          teaser: '{{post.teaser}}',
-          popupContent: '{{post.popupContent}}',
-          date: '{{post.date}}'
-        },
-        'geometry': {
-          'type': 'Point',
-          'coordinates': [
-            {{ post.lng }},
-            {{ post.lat }}
-          ]
-        }
-      },{% endfor %}
-    ]
-  };
-  this._createMap();
+var postsByTag = {
+  {% for tag in site.post_tags %}
+    '{{tag}}' : [
+      {% for post in site.posts %}
+        {% if post.tags contains {{tag}} %}
+          {
+            'type': 'Feature',
+            'properties': {
+              title: "{{ post.title }}",
+              image: processImageLink('{{post.image }}'),
+              link: '{{site.baseurl}}{{post.url}}',
+              teaser: '{{post.teaser}}',
+              popupContent: '{{post.popupContent}}',
+              date: '{{post.date}}',
+              tags: ["{{post.tags | join: '", "'}}"]
+            },
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [
+                {{ post.lng }},
+                {{ post.lat }}
+              ]
+            }
+          },
+        {% endif%} 
+      {% endfor %}
+    ],
+  {% endfor %}
+};
+
+
+
+var postTagsArray = ['{{site.post_tags | join: "', '"}}'];
+
+var AnnaPostMap = function(){
+  var that = this;
+  this.layers = _(postTagsArray)
+                  .map(function(tag){
+                        return L.geoJson(
+                                          {type: 'FeatureCollection', features: postsByTag[tag]},
+                                          {
+                                           onEachFeature: that._onEachFeature.bind(that),
+                                           pointToLayer: that._pointToLayer.bind(that)
+                                          }
+                                        );
+                      });
+
+  this.map = L.map('map', {
+    center: [35.078770, 33.263956],
+    zoom: 9,
+    scrollWheelZoom: false,
+    layers: this.layers
+  });
+
+  L.tileLayer( '{{ site.map-tileset }}', {scrollWheelZoom: false}).addTo(this.map);
+
+  var layerControl = {};
+  _.each(postTagsArray, function(tag, index){
+    layerControl[tag] = that.layers[index];
+  });
+
+  var controlLayers = L.control.layers(null, layerControl, {collapsed: false}).addTo(this.map);
+
+  // this.map.fitBounds(this.geojson.getBounds());
 }
 
 function processImageLink(imageLink){
-  console.log(imageLink.replace(SITE_PROP_LIB['baseurl'], '{{site.baseurl}}'));
   return imageLink.replace(SITE_PROP_LIB['baseurl'], '{{site.baseurl}}');
 }
-
-AnnaPostMap.prototype._createMap = function(){
-  this.map = L.map( 'map', {
-    center: [35.078770, 33.263956],
-    zoom: 9,
-    scrollWheelZoom: false
-  });
-  L.tileLayer( '{{ site.map-tileset }}', {
-    scrollWheelZoom: false,
-  }).addTo( this.map );
-  console.log(this.data);
-  this.geojson = L.geoJson(this.data, {
-    onEachFeature: this._onEachFeature.bind(this)
-  }).addTo( this.map );
-  // this.map.fitBounds(this.geojson.getBounds());
-};
 
 AnnaPostMap.prototype._onEachFeature = function(feature, layer){
   // does this feature have a property named popupContent?
   if (feature.properties && feature.properties.popupContent){
-    console.log(this);
     var popupContent = this._generatePopupContent(feature);
     layer.bindPopup(popupContent);
   }
 };
+
+AnnaPostMap.prototype._pointToLayer = function(feature, latlng){
+  var marker = this._getMarker(feature);
+  return L.marker(latlng, {icon: marker, title: feature.properties.title});
+}
+
+AnnaPostMap.prototype._getMarker = function(feature){
+  if (_.include(feature.properties.tags, "interviews")){
+    return L.AwesomeMarkers.icon({ icon: 'book', prefix: 'fa', markerColor: 'green'});
+  } else if (_.include(feature.properties.tags, "participant photography")){
+    return L.AwesomeMarkers.icon({ icon: 'camera-retro', prefix: 'fa', markerColor: 'red'});
+  } else if (_.include(feature.properties.tags, "team member updates")){
+    return L.AwesomeMarkers.icon({ icon: 'user', prefix: 'fa', markerColor: 'cadetblue'});
+  } else if (_.include(feature.properties.tags, "trekking")){
+    return L.AwesomeMarkers.icon({ icon: 'binoculars', prefix: 'fa', markerColor: 'darkred'});
+  }
+}
 
 AnnaPostMap.prototype._generatePopupContent = function(feature){
   return "<div class='map-popup'>" +
@@ -77,5 +116,4 @@ AnnaPostMap.prototype._generatePopupContent = function(feature){
 
 (function(){
   var postMap = new AnnaPostMap();  
-  console.log("HELLLLLLOOOO");
 })();
